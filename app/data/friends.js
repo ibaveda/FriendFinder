73b9042024f03fc4e3d63d41b4a73f7b03ad91cc
ibaveda <igorbaveda@gmail.com>
2019-02-11 15:33:25 -0700

    first push

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with personal data
+var friendList = [
+    {
+      name: 'Igor Baveda',
+      profilePic: 'https://avatars0.githubusercontent.com/u/7979335?s=400&u=0eb275d0888358d4cf8694f85e3d0f547b6d88c5&v=4',
+      scores: [
+        "3",
+        "4",
+        "5",
+        "2",
+        "5",
+        "3",
+        "4",
+        "4",
+        "4",
+        "3"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
